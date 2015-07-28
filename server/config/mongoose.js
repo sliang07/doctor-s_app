96bb1e550897e2a74588de38c83fe75f3a1939a8
Stanley <sliang07@yahoo.com>
2015-07-27 22:49:45 -0700

    adding mongolab v1.8

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var fs = require('fs');
-var keys = require(__dirname + 'keys.js')
+var keys = require(__dirname + '/keys.js')
 mongoose.connect(keys.remote_db);
 var models_path = __dirname + '/../models'
 fs.readdirSync(models_path).forEach(function(file){
